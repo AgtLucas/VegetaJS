01126658f8553b0ca0ffce1c310fc9239574e730
Lucas Churchill <lucas@agtlucas.com>
2014-05-14 21:47:50 -0300

    Adding sample test

---
+++ sample-test.js
@@ -0,0 +1,31 @@
+// Not working yet
+
+var Bookshelf = require('bookshelf');
+var bluebird = require('bluebird');
+
+// Conect to MySQL
+var MySqlConnection = Bookshelf.initialize({
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'Sayan',
+    charset: 'UTF8_GENERAL_CI'
+  }
+});
+
+MySqlConnection.knex.schema.createTable('users', function (user) {
+  user.charset('UTF8_GENERAL_CI'),
+  user.string('name')
+}).then(function () {
+  var Sayan = MySqlConnection.Model.extend({
+    tableName: 'users'
+  });
+
+  Sayan.forge({
+      name: 'Vegeta'
+    }).save().then(function () {
+      console.log('Kakarotto!');
+  });
+});
